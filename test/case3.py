c1df1471ccfad49214ffe4b469011e60d23639b5
Radenz <radenrifqirahman@gmail.com>
2021-11-24 10:58:21 +0700

    test: Test while dan for loop statement.

--- test/case3.py
+++ test/case3.py
@@ -1,2 +1,2 @@
-def ma():
+def func():
     pass
---
+++ test/case5.py
@@ -0,0 +1,17 @@
+x = 0
+
+while x < 5:
+    print(x)
+    x += 1
+
+if x is 5:
+    print("Success")
+
+if x is not 5:
+    print("Fail")
+
+if x in [5]:
+    print("Sucess")
+
+if x not in [5]:
+    print("Sucess")
